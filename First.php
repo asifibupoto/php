a18479671dda3bd9fd8185e1d473bf4bf5c36c07
asifibupoto <73749383+asifibupoto@users.noreply.github.com>
2020-12-19 20:41:08 +0500

    Add files via upload

---
+++ First.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     echo "hello world <br>"; 
+     
+     echo " kjaljdkajlk <br>";
+     echo "<h1>jalkdjl;akdjlkajdlskaj</h1>"
+
+
+    
+
+
+     ?>
+</body>
+</html>
